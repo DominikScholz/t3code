b34cdebd0bf1e23a4a00be60d67d11fcd78af3ed
Julius Marminge <julius0216@outlook.com>
2026-09-17 21:13:12 -0700

    refactor(mobile): isolate thread row appearance by platform

---
+++ apps/mobile/src/features/threads/thread-list-v2-row-appearance.ts
@@ -0,0 +1,8 @@
+export {
+  THREAD_LIST_V2_MONO_FONT,
+  THREAD_LIST_V2_ROW_CONTENT_CLASS_NAME,
+  THREAD_LIST_V2_ROW_DIVIDERS,
+  selectedThreadRowColors,
+  getThreadListV2NewBranchMenuTitle,
+  getThreadListV2RowAppearance,
+} from "./thread-list-v2-row-appearance.ios";
